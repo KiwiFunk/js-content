b00587d5dd7e7459019b9293a416290353c87f52
nathan.blanch <Nathan.Blanch@os.uk>
2025-11-05 15:18:02 +0000

    add challenge 3

---
+++ 3. Magic Eight Ball/magic8Ball.js
@@ -0,0 +1,9 @@
+function magicEightBall() {
+    // Your code here
+}
+
+function logMagicEightBall(userQuestion, userName) {
+    // Your code here
+}
+
+module.exports = { magicEightBall, logMagicEightBall };
