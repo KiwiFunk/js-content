9cd86c7942884336b6cf0105e9f16d553f53bd2c
Alex Price <alex_p97@live.com>
2025-12-17 14:47:40 +0000

    Created tests for getSleepHours

--- 6. Sleep Calculator/sleepCalculator.test.js
+++ 6. Sleep Calculator/sleepCalculator.test.js
@@ -5,7 +5,37 @@ const {
     calculateSleepDebt
 } = require("./sleepCalculator")
 
-// This is just a placeholder to prevent test suite errors
-describe.skip('', () => {
-    test('', () => { });
+describe("Sleep Calculator", () => {
+    // Tests for getSleepHours
+    describe("getSleepHours", () => {
+        // Tests for this function
+        test('returns correct weekly total for 8 hours/night', () => {
+            expect(getSleepHours('monday')).toBe(8);
+            expect(getSleepHours('tuesday')).toBe(7);
+            expect(getSleepHours('wednesday')).toBe(6);
+            expect(getSleepHours('thursday')).toBe(7);
+            expect(getSleepHours('friday')).toBe(5);
+            expect(getSleepHours('saturday')).toBe(9);
+            expect(getSleepHours('sunday')).toBe(8);
+        });
+
+        test('returns error for invalid day', () => {
+            expect(getSleepHours('invalidDay')).toBeInstanceOf(Error);
+        });
+    });
+
+    // Tests for getActualSleepHours
+    describe.skip("getActualSleepHours", () => {
+
+    });
+
+    // Tests for getIdealSleepHours
+    describe.skip("getIdealSleepHours", () => {
+
+    });
+
+    // Tests for calculateSleepDebt
+    describe.skip("calculateSleepDebt", () => {
+
+    });
 });
